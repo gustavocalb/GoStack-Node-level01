405b190d00fb2aaf7c41fbfd118644966c4f8b31
Gustavo Carvalho <gugacalb@gmail.com>
2021-05-10 16:40:25 -0300

    Add files via upload

---
+++ Challenge/src/app.js
@@ -0,0 +1,109 @@
+const express = require("express");
+const cors = require("cors");
+
+const { v4: uuid, validate: isUuid } = require('uuid');
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+const repositories = [];
+
+function validateRepositoriesId(req, res, next) {
+  const { id } = req.params
+
+  if (!isUuid(id)) {
+    return res.status(400).json({ error: 'Invalid repositorie ID' })
+  }
+
+  return next()
+}
+
+app.use('/repositories/:id', validateRepositoriesId)
+
+app.get("/repositories", (req, res) => {
+  return res.json(repositories)
+});
+
+app.post("/repositories", (req, res) => {
+  const { title, url, techs } = req.body
+
+  const repositorie = {
+    id: uuid(),
+    title, 
+    url,
+    techs,
+    likes: 0
+  }
+
+  repositories.push(repositorie)
+
+  return res.json(repositorie)
+});
+
+app.put("/repositories/:id", (req, res) => {
+  const { id } = req.params;
+  const { title, url, techs } = req.body;
+
+  const repositorieIndex = repositories.findIndex(repositorie => repositorie.id = id)
+
+  if (repositorieIndex < 0) {
+    return res.status(400).json({ error: 'Repositorie not found' })
+  }
+
+  const repositorie = {
+    id,
+    title,
+    url,
+    techs
+  }
+
+  repositories[repositorieIndex] = repositorie
+
+  return res.json(repositorie)
+
+});
+
+app.delete("/repositories/:id", (req, res) => {
+  const { id } = req.params;
+
+  const repositorieIndex = repositories.findIndex(repositorie => repositorie.id = id)
+
+  if (repositorieIndex < 0) {
+    return res.status(400).json({ error: 'Repositorie not found' })
+  }
+
+  repositories.splice(repositorieIndex, 1)
+
+  return res.status(204).send()
+});
+
+app.post("/repositories/:id/like", (req, res) => {
+  const { id } = req.params;
+
+  const repositorieIndex = repositories.findIndex(repositorie => repositorie.id === id)
+
+  if (repositorieIndex < 0) {
+    return res.status(400).json({ error: 'Repositorie not found' })
+  }
+
+  const { title, url, techs, likes } = repositories[repositorieIndex]
+
+  const repositorie = {
+    id,
+    title,
+    url,
+    techs,
+    likes: likes + 1
+  }
+
+  repositories[repositorieIndex] = repositorie
+
+  console.log(repositorie)
+
+  return res.json(repositorie)
+
+});
+
+module.exports = app;
---
+++ Nivel-01/src/index.js
@@ -0,0 +1,94 @@
+const express = require('express');
+const { v4: uuidv4 } = require('uuid')
+const { isUuid } = require('uuidv4')
+
+const app = express();
+
+app.use(express.json())
+
+const projects = []
+
+function logRequests(req, res, next) {
+  const { method, url } = req
+
+  const logLabels = `[${method.toUpperCase()}] ${url}`
+
+  console.time(logLabels)
+
+  next()
+
+  console.timeEnd(logLabels)
+}
+
+function validateProjectId(req, res, next) {
+  const { id } = req.params;
+
+  if (!isUuid(id)) {
+    return res.status(400).json({ error: 'Invalid project ID' })
+  }
+
+  return next()
+}
+
+app.use(logRequests)
+app.use('/projects/:id', validateProjectId)
+
+app.get('/projects', (req, res) => {
+  const { title } = req.query;
+
+  const results = title
+  ? projects.filter(project => project.title.includes(title))
+  : projects;
+
+  return res.json(results)
+});
+
+app.post('/projects', (req, res) => {
+  const { title, owner } = req.body
+
+  const project = { id: uuidv4(), title, owner }
+
+  projects.push(project)
+
+  return res.json(project)
+})
+
+app.put('/projects/:id', (req, res) => {
+  const { id } = req.params
+  const { title, owner } = req.body
+
+  const projectIndex = projects.findIndex(project => project.id === id)
+
+  if (projectIndex < 0) {
+    return res.status(400).json({ error: 'Projeto not found' })
+  }
+
+  const project = {
+    id,
+    title,
+    owner
+  }
+
+  projects[projectIndex] = project
+
+  return res.json(project)
+})
+
+app.delete('/projects/:id', (req, res) => {
+  const { id } = req.params;
+
+  const projectIndex = projects.findIndex(project => project.id === id)
+
+  if (projectIndex < 0) {
+    return res.status(400).json({ error: 'Projeto not found' })
+  }
+
+  projects.splice(projectIndex, 1)
+
+  return res.status(204).send()
+})
+
+
+app.listen(3333, () => {
+  console.log('🚀 Back-end started')
+})
